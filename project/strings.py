15d2b624d5a861c7f60f662b82a2a8b6f0eb27eb
André Moleiro <82454876+AndMoleiro@users.noreply.github.com>
2022-03-05 09:26:37 +0000

    d

--- project/strings.py
+++ project/strings.py
@@ -2,10 +2,17 @@ print("Today is a good day to learn Python")
 print('Python is fun')
 print("Python's string  are easy to use")
 print('We can even include "quotes" in strings')
-print("hello" + " world")
+print("Hello" + " world")
 greeting = "Hello"
 # name = "Bruce"
-name = input("Please enter your name ")
+#name = input("Please enter your name ")
+name = "Tim"
 
 # if we want a space we can add that too
 print(greeting + ' ' + name)
+
+age = 24
+print(age)
+
+print(type(greeting))
+print(type(age))
