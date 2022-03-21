37fb1ed6dce8b2d0a49bd9c4c6aa985e449296db
André Moleiro <82454876+AndMoleiro@users.noreply.github.com>
2022-03-21 11:13:32 +0000

    g

--- project/section_4/challenge.py
+++ project/section_4/challenge.py
@@ -1,20 +1,49 @@
-available_inputs = [0, 1, 2, 3, 4, 5]
-chosen_inputs = []
+# print("Please choose your option from the list below:")
+# print("1:\tLearn Python")
+# print("2:\tLearn Java")
+# print("3:\tGo swimming")
+# print("4:\tHave dinner")
+# print("5:\tGo to bed")
+# print("0:\tExit")
+#
+# while True:
+#     choice = input()
+#
+#     if choice == "0":
+#         break
+#     elif choice in "12345":
+#         print(f"You chose {choice}")
+#
+#     else:
+#         print("Please choose your option from the list below:")
+#         print("1:\tLearn Python")
+#         print("2:\tLearn Java")
+#         print("3:\tGo swimming")
+#         print("4:\tHave dinner")
+#         print("5:\tGo to bed")
+#         print("0:\tExit")
 
-while True:
-    input_number = input("Please choose your option from the list below:"
-                         "\n1.\tLearn Python"
-                         "\n2.\tLearn Java"
-                         "\n3.\tGo swimming"
-                         "\n4.\tHave dinner"
-                         "\n5.\tGo to bed"
-                         "\n0.\tExit"
-                         "\n")
-    if input_number == 0:
-        print("Exiting")
-        break
 
-    chosen_inputs += input_number
-    print("activities chosen: " + str(chosen_inputs))
+# alternative version
+
+# choice = "-"
+# while choice != "0":
+#     if choice in "12345":
+#         print(f"You chose {choice}")
+#
+#     else:
+#         print("Please choose your option from the list below:")
+#         print("1:\tLearn Python")
+#         print("2:\tLearn Java")
+#         print("3:\tGo swimming")
+#         print("4:\tHave dinner")
+#         print("5:\tGo to bed")
+#         print("0:\tExit")
+#
+#     choice = input()
 
 
+for x in range(30):
+    if x % 3 == 0 or x % 5 == 0:
+        continue
+    print(x)
