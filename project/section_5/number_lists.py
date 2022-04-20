5b41a01ace71d4149de865bb6c58388c7fbe9600
André Moleiro <82454876+AndMoleiro@users.noreply.github.com>
2022-04-20 18:10:06 +0100

    Changes

--- project/section_5/number_lists.py
+++ project/section_5/number_lists.py
@@ -1,15 +1,29 @@
+# even = [2, 4, 6, 8]
+# odd = [1, 3, 5, 7, 9]
+#
+# print(min(even))
+# print(max(even))
+# print(min(odd))
+# print(max(odd))
+#
+# print()
+# print(len(even))
+# print(len(odd))
+#
+#
+# print()
+# print("mississippi".count("s"))
+
+empty_list = []
 even = [2, 4, 6, 8]
 odd = [1, 3, 5, 7, 9]
 
-print(min(even))
-print(max(even))
-print(min(odd))
-print(max(odd))
+numbers = even + odd
+print(numbers)
 
-print()
-print(len(even))
-print(len(odd))
+sorted_numbers = sorted(numbers)
+print(sorted_numbers)
 
+digits = sorted("432985617")
+print(digits)
 
-print()
-print("mississippi".count("s"))
